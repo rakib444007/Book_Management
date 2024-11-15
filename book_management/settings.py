b0acd1603bcbaa76306da3697b40d6c09037fe85
rakib444007 <rh444007@gmail.com>
2024-11-16 00:54:50 +0600

    image url changes

--- book_management/settings.py
+++ book_management/settings.py
@@ -142,6 +142,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 # Base url to serve media files
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
