d0b7614062a2aa65477f097ce4d08ad3e1f93bd8
rakib444007 <rh444007@gmail.com>
2024-11-17 12:22:00 +0600

    debug: ture

--- book_management/settings.py
+++ book_management/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY =env("SECRET")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['book-management-Ogw3.onrender.com','localhost','127.0.0.1']
 CSRF_TRUSTED_ORIGINS = ['https://book-management-Ogw3.onrender.com']
